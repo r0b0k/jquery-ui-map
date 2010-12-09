d91a572c63a369f339122385ea3cd91834ecc68b
johansalllarsson@gmail.com <johansalllarsson@gmail.com@26b9fb5c-081e-8f47-c0ad-8557b4f7f1b3>
2010-12-09 23:44:01 +0000

    Fixed clash with .map so it's now gmap. Fixed RDFa method and microformats method... and some other stuff

--- trunk/ui/jquery.ui.map.js
+++ trunk/ui/jquery.ui.map.js
@@ -1,5 +1,5 @@
 /*
- * jQuery UI Map 0.1
+ * jQuery UI Map 0.1.1
  *
  * Author: Johan Säll Larsson
  *
@@ -9,167 +9,284 @@
  */
 
 ( function($) {
-		
-	var map, markers = [];
-
-	$.widget( "ui.map", {
+	
+	var maps = [], markers = [], watchIds = [], id;
+	
+	$.widget( "ui.gmap", {
 			
 			options: {
-				fallbackInitialLatLon: new google.maps.LatLng(0.0, 0.0),
+				backgroundColor : null,
+				center: new google.maps.LatLng(0.0, 0.0),
+				disableDefaultUI: false,
+				disableDoubleClickZoom: false,
+				draggable: true,
+				draggableCursor: null,
+				draggingCursor: null,
+				keyboardShortcuts: true,
+				mapTypeControl: true,
+				mapTypeControlOptions: null,
 				mapTypeId: google.maps.MapTypeId.ROADMAP,
-				autoDetectGeoLocation: true, 
-				language: 'en',
-				region: 'us',
-				zoom: 5
+				navigationControl: true,
+				navigationControlOptions: null,
+				noClear: false,
+				scaleControl: false,
+				scaleControlOptions: null,
+				scrollwheel: false,
+				//streetView: new google.maps.StreetViewPanorama(),
+				streetViewControl: true,
+				streetViewControlOptions: null,
+				zoom: 5,
+				callback: null,
+				map: null
 			},
 			
 			_create: function() {
-				map = new google.maps.Map( document.getElementById(this.element.attr('id')), this.options );
-				map.setCenter(this.options.fallbackInitialLatLon);
-				this._initializeGeoPositioning();
+				id = this.element.attr('id');
 			},
 			
-			_initializeGeoPositioning: function() {
+			_init: function() {
+					var o = this.options;
+					o.map = maps[id] = new google.maps.Map( document.getElementById(id), o );
+					markers[id] = new Array;
+					watchIds[id] = new Array;
+					if ( $.isFunction(o.callback) ) {
+						o.callback.call(this);	
+					}
+			},
+			
+			getCurrentPosition: function(successCallback, errorCallback, notSupportedCallback, opts) {
 				var self = this;
-				if ( this.options.autoDetectGeoLocation ) {
-					if ( navigator.geolocation ) {
-						navigator.geolocation.getCurrentPosition ( 
-							function(position) {
-								map.setCenter( new google.maps.LatLng(position.coords.latitude, position.coords.longitude) );
-							}, 
-							function(error) {
-								if ( error.code == error.TIMEOUT ) {
-									self._initializeGeoPositioning();
-								} else {
-									// TODO: fix this, and remove reminder
-									// alert('Fix this in a later version! :)');
-								}
-							}, 
-							{ maximumAge :600000 } 
-						);
+				if ( navigator.geolocation ) {
+					if ( $.isFunction(successCallback) && $.isFunction(errorCallback) ) {
+						navigator.geolocation.getCurrentPosition ( successCallback, errorCallback, opts );
 					} else {
-						// TODO: either display dialog or let user do whatever here
+						navigator.geolocation.getCurrentPosition ( function(position) {
+							maps[id].setCenter( new google.maps.LatLng(position.coords.latitude, position.coords.longitude) );								
+						});
+					}
+				} else {
+					if ( $.isFunction(notSupportedCallback) ) {
+						notSupportedCallback.call(this);
 					}
 				}
 			},
 			
-			addSidebar: function(_element, _position) {
-				function getPosition (position) {
-					switch ( position ) {
-						case 'tl':
-							return google.maps.ControlPosition.TOP_LEFT;
-						case 'tc':
-							return google.maps.ControlPosition.TOP_CENTER;
-						case 'tr':
-							return google.maps.ControlPosition.TOP_RIGHT;
-						case 'bl':
-							return google.maps.ControlPosition.BOTTOM_LEFT;
-						case 'bc':
-							return google.maps.ControlPosition.BOTTOM_CENTER;
-						case 'br':
-							return google.maps.ControlPosition.BOTTOM_LEFT;
-						case 'lt':
-							return google.maps.ControlPosition.LEFT_TOP;
-						case 'lc':
-							return google.maps.ControlPosition.LEFT_CENTER;
-						case 'lb':
-							return google.maps.ControlPosition.LEFT_BOTTOM;
-						case 'rt':
-							return google.maps.ControlPosition.RIGHT_TOP;
-						case 'rc':
-							return google.maps.ControlPosition.RIGHT_CENTER;
-						case 'rb':
-							return google.maps.ControlPosition.RIGHT_BOTTOM;
-					}
-				}
-				map.controls[getPosition(_position.toLowerCase())].push(document.getElementById(_element));
-			},
-			
-			addMarker: function( _latitude, _longitude, _options ) {
-				
-				var options = _options || {};
-				options = jQuery.extend( { map: map, position: new google.maps.LatLng(_latitude, _longitude) }, options);
-				
-				if ( options.link != null ) {
-					options.clickable = true;
-					options.title = options.link.html();
-				}
-				
-				var marker = new google.maps.Marker( options );
-				
-				if ( options.link != null ) {
-					var infowindow = new google.maps.InfoWindow({ content: options.title });
-					options.link.click( function() {
-						google.maps.event.trigger(marker, 'click');
-						map.setCenter(marker.position);
-						return false;
-					});
-					google.maps.event.addListener(marker, "click", function() { 
-    					infowindow.open(map,marker);
-					});
-				}
-				
-				markers.push( marker );
-				
-			},
-			
-			loadJSON: function( _url, _data ) {
+			watchPosition: function(successCallback, errorCallback, notSupportedCallback, opts) {
 				var self = this;
-				$.getJSON( _url, _data, function(data) { 
-					$.each( data.markers, function(i, marker) {
-						self.addMarker(marker.lat, marker.lng, { title: marker.title });
+				if ( navigator.geolocation ) {
+					if ( $.isFunction(successCallback) && $.isFunction(errorCallback) ) {
+						watchIds[id].push(navigator.geolocation.watchPosition ( successCallback, errorCallback, opts ));
+					} else {
+						watchIds[id].push(navigator.geolocation.watchPosition ( function(position) { maps[id].setCenter( new google.maps.LatLng(position.coords.latitude, position.coords.longitude) ); }));
+					}
+				} else {
+					if ( $.isFunction(notSupportedCallback) ) {
+						notSupportedCallback.call(this);
+					}
+				}
+			},
+			
+			sidebar: function(panel, position) {
+				maps[id].controls[position].push(document.getElementById(panel));
+			},
+			
+			addMarker: function( markerOptions, callback ) {
+				var marker = new google.maps.Marker( jQuery.extend( { 'map': maps[id] }, markerOptions) );
+				if ( $.isFunction(callback) ) {
+					callback.call(this, maps[id], marker);
+				}
+				markers[id].push( marker );
+				return marker;
+			},
+			
+			addInfoWindow: function (marker, infoWindowOptions) {
+				var self = this;
+				var infowindow = new google.maps.InfoWindow(infoWindowOptions);
+				google.maps.event.addListener(marker, 'click', function() { 
+    				infowindow.open(maps[id], marker);
+					maps[id].panTo(marker.position);
+				});
+			},
+			
+			loadJSON: function( url, data, callback ) {
+				var self = this;
+				$.getJSON( url, data, function(data) { 
+					$.each( data.markers, function(i, m) {
+						if ( $.isFunction(callback) ) {
+							callback.call(this, i, m);
+						} else {
+							var markerOpts = { 'map': maps[id], 'position': new google.maps.LatLng(m.lat, m.lng), 'title': m.title };
+							self.addMarker(markerOpts);
+						}
 					});
 				});
 			},
 			
-			loadHTML: function ( _className, _schema ) {
+			loadHTML: function ( type, clazz, callback ) {
 				var self = this;
-				switch ( _schema ) {
+				switch ( type ) {
 					case 'rdfa':
-						$('.'+_className).each( function() {
-							var latlng = ($(this).attr("content")).split(',');
-							self.addMarker( latlng[0], latlng[1], { link: $(this).find('a') });
+						var geoPoints = [];
+						$('.'+clazz+' [property="geo:lat_long"]').each( function() {
+							geoPoints.push(($(this).attr('content')).split(','));												
+						});
+						$('.'+clazz).each( function(index) {
+							var object = $(this);
+							var markerOpts = { 'map': maps[id], 'position': new google.maps.LatLng(geoPoints[index][0], geoPoints[index][1]) };
+							if ( $.isFunction(callback) ) {
+								callback.call(this, markerOpts, object, index);
+							} else {
+								self.addMarker( 
+									markerOpts, 
+									function(map, marker) {
+										var summary = object.find('.summary');
+										if ( summary != null ) {
+											self.addInfoWindow(marker, { 'content': summary.html() });
+											summary.click( function() {
+												google.maps.event.trigger(marker, 'click');
+												map.panTo(marker.position);
+												return false;
+											});
+										}
+									} 
+								);	
+							}
 						});
 					break;
 					case 'microformat':
-						$('.'+_className).each( function() {
-							self.addMarker( $(this).find('.latitude').attr('title'), $(this).find('.longitude').attr('title'), { link: $(this).find('a') } );
+						$('.'+clazz).each( function(index) {
+							var object = $(this);
+							var markerOpts = { 'map': maps[id], 'position': new google.maps.LatLng(object.find('.latitude').attr('title'), object.find('.longitude').attr('title')) };
+							if ( $.isFunction(callback) ) {
+								callback.call(this, markerOpts, object, index);
+							} else {
+								self.addMarker( 
+									markerOpts, 
+									function(map, marker) {
+										var summary = object.find('.summary');
+										if ( summary != null ) {
+											self.addInfoWindow(marker, { 'content': summary.html() });
+											summary.click( function() {
+												google.maps.event.trigger(marker, 'click');
+												map.panTo(marker.position);
+												return false;
+											});
+										}
+									} 
+								);
+							}
 						});
 					break;
+					//FIXME: Seriously fix this
 					case 'microdata':
-						$('.'+_className).each( function() {
-							var link = $(this).find('a');
-							$(this).children().each( function() {
+						$('.'+clazz).each( function() {
+							var object = $(this);
+							$(object).children().each( function(index) {
 								if ( $(this).attr('itemprop') == 'geo' ) {
 									var latlng = $(this).html().split(';');
-									self.addMarker(latlng[0], latlng[1], { link: link } );
+									var markerOpts = { 'map': maps[id], 'position': new google.maps.LatLng(latlng[0], latlng[1]) };
+									if ( $.isFunction(callback) ) {
+										callback.call(this, markerOpts, object, index);
+									} else {
+										self.addMarker( 
+											markerOpts, 
+											function(map, marker) {
+												self.addInfoWindow(marker, { 'content': $(object).get(0) });
+												object.find('a.link').click( function() {
+													google.maps.event.trigger(marker, 'click');
+													map.panTo(marker.position);
+													return false;
+												});
+											} 
+										);
+									}
+									
 								}
 							});
 						});
 					break;
 				}
+				
 			},
 			
-			loadDirections: function(_origin, _destination, _travelMode, _directionId) { 
-				var directionsDisplay = new google.maps.DirectionsRenderer({ map: map, panel: document.getElementById(_directionId)});
+			//FIXME: Should be diff. params
+			loadDirections: function(panel, origin, destination, travelMode) { 
+				var directionsDisplay = new google.maps.DirectionsRenderer({ 'map': maps[id], 'panel': document.getElementById(panel)});
 				var directionsService = new google.maps.DirectionsService();
-				directionsService.route( { origin:_origin, destination:_destination, travelMode: _travelMode, provideRouteAlternatives : true, region: this.options.region } , function(response, status) {
-					if ( status == google.maps.DirectionsStatus.OK ) {
-						directionsDisplay.setDirections(response);
+				directionsService.route( { 'origin':origin, 'destination':destination, 'travelMode': travelMode, 'provideRouteAlternatives' : true }, 
+					function(response, status) {
+						if ( status == google.maps.DirectionsStatus.OK ) {
+							directionsDisplay.setDirections(response);
+						} else {
+							alert('Couldnt find directions, ' + status );
+						}
+					}
+				);
+			},
+			
+			loadStreetViewPanorama: function(panel, streetViewPanoramaOptions) {
+				var panorama = new google.maps.StreetViewPanorama(document.getElementById(panel), streetViewPanoramaOptions);
+				maps[id].setStreetView(panorama);
+			},
+			
+			search: function(request, successCallback, errorCallback) {
+				var geocoder = new google.maps.Geocoder();
+				geocoder.geocode( request, function(results, status) {
+					if ( status == google.maps.GeocoderStatus.OK ) {
+						if ( $.isFunction(successCallback) ) {
+							successCallback.call(this, results);
+						} else {
+							maps[id].setCenter(results[0].geometry.location);
+						}
 					} else {
-						alert('Couldnt find directions, ' + status );
+						if ( $.isFunction(errorCallback) ) {
+							errorCallback.call(this, status);
+						}
 					}
 				});
 			},
 			
 			clearMarkers: function() {
-				for ( var i = 0; i < markers.length; i++ ) {
-					markers[i].setMap( null );
+				for ( var i = 0; i < markers[id].length; i++ ) {
+					markers[id][i].setMap( null );
 				}
-				markers = new Array();
+				markers[id] = new Array();
 			},
 			
+			clearWatches: function() {
+				if ( navigator.geolocation ) {
+					for ( var i = 0; i < watchIds[id].length; i++ ) {
+						navigator.geolocation.clearWatch([id][i]);
+					}
+					watchIds[id] = new Array();
+				}
+			},
+			
+			//TODO: Add the rest of the options
+			_setOption: function(key, value) {
+				switch (key) {
+					case "center":
+						this.options.center = value;
+                    	maps[id].setCenter(value);
+					break;
+					case 'mapTypeId':
+						this.options.mapTypeId = value;
+						maps[id].setMapTypeId(value);
+					break;
+					case 'zoom':
+						this.options.zoom = value;
+                    	maps[id].setZoom(value);
+					break;
+				}
+				$.Widget.prototype._setOption.apply(this, arguments);
+			},
+			
+			//FIXME: Google Map won't go away
 			destroy: function() {
+				this.clearWatches();
+				this.clearMarkers();
+				maps[id] = null;
+				this.options.map = null;
 				$.Widget.prototype.destroy.call( this );
 			}
 			

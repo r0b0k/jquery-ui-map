edbc5d443b11c6bd25981be0639521c365c7f660
johansalllarsson@gmail.com <johansalllarsson@gmail.com@26b9fb5c-081e-8f47-c0ad-8557b4f7f1b3>
2011-08-11 23:44:14 +0000

    sdf

--- tags/3.0-alpha/ui/jquery.ui.map.js
+++ tags/3.0-alpha/ui/jquery.ui.map.js
@@ -1,26 +1,8 @@
  /*!
  * jQuery UI Google Map 3.0-alpha
  * http://code.google.com/p/jquery-ui-map/
- *
  * Copyright (c) 2010 - 2011 Johan Säll Larsson
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  *
  * Depends:
  *      jquery.ui.core.js
@@ -47,11 +29,12 @@
 		 */
 		_create: function() {
 			this.options.center = $.ui.gmap._unwrapLatLng(this.options.center);
-			var a = $.ui.gmap.instances[this.element.attr('id')] = { map: new google.maps.Map( this.element[0], this.options ), markers: [], services: [], overlays: [] };
-			google.maps.event.addListenerOnce(a.map, 'bounds_changed', function() {
-				a.trigger('init', a.map);
+			var a = this.element;
+			var b = $.ui.gmap.instances[a.attr('id')] = { map: new google.maps.Map( a[0], this.options ), markers: [], services: [], overlays: [] };
+			google.maps.event.addListenerOnce(b.map, 'bounds_changed', function() {
+				a.trigger('init', b.map);
 			});
-			return $(a.map);
+			return $(b.map);
 		},
 		
 		/**
